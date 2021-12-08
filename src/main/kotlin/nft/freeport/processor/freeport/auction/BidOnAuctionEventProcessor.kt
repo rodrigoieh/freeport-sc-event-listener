78e03ed7df001a84c20cf68833a2e8a421ca3076
Vadim Hleif <vadzim.hleif@gmail.com>
2021-12-08 19:13:20 +0300

    feat: filter invalid sc events

--- src/main/kotlin/nft/freeport/processor/freeport/auction/BidOnAuctionEventProcessor.kt
+++ src/main/kotlin/nft/freeport/processor/freeport/auction/BidOnAuctionEventProcessor.kt
@@ -2,19 +2,20 @@ package nft.freeport.processor.freeport.auction
 
 import nft.freeport.listener.event.BidOnAuction
 import nft.freeport.listener.event.SmartContractEventData
-import nft.freeport.processor.freeport.FreeportEventProcessor
+import nft.freeport.processor.freeport.FreeportExistingNftRelatedEventProcessor
+import nft.freeport.processor.freeport.nft.NftEntity
 import org.slf4j.LoggerFactory
 import java.time.Instant
 import javax.enterprise.context.ApplicationScoped
 import javax.transaction.Transactional
 
 @ApplicationScoped
-class BidOnAuctionEventProcessor : FreeportEventProcessor<BidOnAuction> {
+class BidOnAuctionEventProcessor : FreeportExistingNftRelatedEventProcessor<BidOnAuction> {
     private val log = LoggerFactory.getLogger(javaClass)
     override val supportedClass = BidOnAuction::class.java
 
     @Transactional
-    override fun process(eventData: SmartContractEventData<out BidOnAuction>) = with(eventData) {
+    override fun process(eventData: SmartContractEventData<out BidOnAuction>, nft: NftEntity) = with(eventData) {
         val auction = AuctionEntity.findActive(event.seller, event.nftId)
         if (auction == null) {
             log.warn("Active auction not found for seller ${event.seller} and nftId ${event.nftId}")
